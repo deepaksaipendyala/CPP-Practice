2e0c2bd8b6ad1a67e31f9825f2c3209236483cc3
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2022-08-26 15:27:24 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  std::cout << "Hello World!\n"; // If we use namespace std no usde of std::
+  cout<<"hello world!\nHello world 2.0!"<<endl; // lik dis
+  
+}
